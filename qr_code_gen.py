2f4802a7728dce758b67ddd3f38b673687c896d6
Tom4sCruz <tomascruz2300@gmail.com>
2025-01-26 14:54:38 +0000

    updated qr_code_gen.py

--- qr_code_gen.py
+++ qr_code_gen.py
@@ -3,11 +3,17 @@ import qrcode
 print("Type whatever you want to generate a QR Code (Text, URL, ...):")
 txt_to_qr = input()
 
+
 qr = qrcode.QRCode(border=1)
 qr.add_data(txt_to_qr)
 qr.make(fit=True)
 
 qr.print_ascii(invert=True)
+
+if '/' in txt_to_qr:
+    txt_to_qr = txt_to_qr.replace('/', '|')
+if 'http' in txt_to_qr:
+    txt_to_qr = txt_to_qr[txt_to_qr.find('www')+4:]
 
 img = qr.make_image()
 img.save(f"QR_Codes/{txt_to_qr[:10]}{'...' if len(txt_to_qr) > 10 else ''}_QRcode.png")
